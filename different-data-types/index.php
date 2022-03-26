1dff6d576cabe74d07bf7a7490e6afc3596756e3
paulyarangga11 <paulyarangga07@gmail.com>
2022-03-26 11:03:10 +0900

    PHP Tutorials

---
+++ different-data-types/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //string
+    $name = 'Coding is fun';
+
+    //Integer
+    $namee = 20;
+    
+    //Float
+    $name = 20.5784;
+    
+    //Boolean
+    true = 1 
+    false = 0
+
+    //Array
+    $name = array("Daniel","Dennis","Michael");
+    echo $names['1'];
+    ?>
+</body>
+</html>
---
+++ internal-functions/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo str_word_count("Hi Daniel");
+        echo strrev("Hi Daniel");
+        echo strpos("Hi Daniel");
+        echo str_replace("Daniel","Jason","Hi Daniel");
+    ?>
+    
+</body>
+</html>
